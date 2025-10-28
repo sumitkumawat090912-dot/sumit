4ff1e26cdaa7ce7c921713c2a2725d91df459dcb
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-10-28 18:55:40 +0530

    main

---
+++ modules/vars.py
@@ -0,0 +1,34 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "20114039"))
+API_HASH = environ.get("API_HASH", "87297b8f3cc8fc9bbce591ad30da5896")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8267941189:AAFkEX73f3QfoycNBae8afg-C3EmfthOAoI")
+
+OWNER = int(environ.get("OWNER", "6053889491"))
+CREDIT = environ.get("CREDIT", "@ll_kumawat_ll")
+cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6053889491').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '6053889491').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
+# .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
+api_url = "http://master-api-v3.vercel.app/"
+api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
+# .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
+
+
+
+
+
+
+
+

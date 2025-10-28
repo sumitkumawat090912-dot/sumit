0de92d82e76d3e427e3ff16d8851decb91fdf8de
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-10-28 19:52:56 +0530

    main

--- modules/vars.py
+++ modules/vars.py
@@ -5,16 +5,16 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20114039"))
 API_HASH = environ.get("API_HASH", "87297b8f3cc8fc9bbce591ad30da5896")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8267941189:AAFkEX73f3QfoycNBae8afg-C3EmfthOAoI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8132042900:AAGDycGOYTqIKlUcQx1Qv3w-k63bgeqbGuk")
 
-OWNER = int(environ.get("OWNER", "6053889491"))
+OWNER = int(environ.get("OWNER", "8251679097"))
 CREDIT = environ.get("CREDIT", "@ll_kumawat_ll")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6053889491').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8251679097').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6053889491').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8251679097').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
